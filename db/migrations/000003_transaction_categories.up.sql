0c38efff8f4d04cf6283656aeeb9222277ea5279
Maulana Zain <maulanadezain@gmail.com>
2023-05-09 08:07:09 +0700

    feat: add dummy data to migration

--- db/migrations/000003_transaction_categories.up.sql
+++ db/migrations/000003_transaction_categories.up.sql
@@ -2,6 +2,23 @@ CREATE TABLE IF NOT EXISTS transaction_categories (
   id SERIAL PRIMARY KEY,
   name VARCHAR NOT NULL,
   description VARCHAR DEFAULT '',
-  created_at TIMESTAMPTZ,
+  created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMPTZ
 );
+
+INSERT INTO transaction_categories (name, description)
+VALUES('Food & Beverage', 'Food & Beverage description'),
+  ('Shopping', 'Shopping description'),
+  ('Transportation', 'Transportation description'),
+  ('Bill & Utility', 'Bill & Utility description'),
+  ('Travel', 'Travel description'),
+  ('Family', 'Family description'),
+  ('Education', 'Education description'),
+  ('Investation', 'Investation description'),
+  ('Business', 'Business description'),
+  ('Gift', 'Gift description'),
+  ('Freelance', 'Freelance description'),
+  ('Fashion', 'Fashion description'),
+  ('Tool', 'Tool description'),
+  ('Book', 'Book description'),
+  ('Health', 'Health description');
