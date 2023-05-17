e64335b91dc6af1c7f16e123a2ea769e8dd22b2e
Maulana Zain <maulanadezain@gmail.com>
2023-05-17 13:29:27 +0700

    feat: generate mock repository

--- internal/app/service/transaction_category_test.go
+++ internal/app/service/transaction_category_test.go
@@ -1,15 +1,39 @@
 package service
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/14mdzk/dev_finance/internal/app/model"
+	"github.com/14mdzk/dev_finance/internal/app/schema"
 	"github.com/14mdzk/dev_finance/internal/app/service/mocks"
 	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTransactionCategoryService_BrowseAll(t *testing.T) {
 	ctrl := gomock.NewController(t)
 
 	repo := mocks.NewMockITransactionCategoryRepository(ctrl)
-	repo.EXPECT().Browse()
+	repo.EXPECT().Browse(fmt.Sprintf(`
+		LIMIT %d 
+		OFFSET %d
+	`, 2, 0)).Return([]model.TransactionCategory{
+		{
+			ID:          1,
+			Name:        "Food & Beverage",
+			Description: "Food & Beverage description",
+		},
+		{
+			ID:          2,
+			Name:        "Shopping",
+			Description: "Shopping description",
+		},
+	}, nil)
+
+	categoryService := NewTransactionCategoryService(repo)
+	categories, err := categoryService.BrowseAll(schema.PaginationReq{Page: 1, PageSize: 2})
+	total := len(categories)
+	assert.Equal(t, total, 2)
+	assert.NoError(t, err)
 }
